aed432f08ba8603e7fb729d8a5d8a9d28aa1693b
Joe Karasek <joseph.karasek@gmail.com>
2019-11-19 22:39:13 -0800

    feat: add project cards to homepage

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -1,75 +1,45 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import { Container, Card } from 'components/common'
-import starIcon from 'assets/icons/star.svg'
-import forkIcon from 'assets/icons/fork.svg'
-import { Wrapper, Grid, Item, Content, Stats } from './styles'
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { Container, Card } from 'components/common';
+import starIcon from 'assets/icons/star.svg';
+import forkIcon from 'assets/icons/fork.svg';
+import { Wrapper, Grid } from './styles';
 
 export const Projects = () => {
-  // const {
-  //   github: {
-  //     viewer: {
-  //       repositories: { edges },
-  //     },
-  //   },
-  // } = useStaticQuery(
-  //   graphql`
-  //     {
-  //       github {
-  //         viewer {
-  //           repositories(
-  //             first: 8
-  //             orderBy: { field: STARGAZERS, direction: DESC }
-  //           ) {
-  //             edges {
-  //               node {
-  //                 id
-  //                 name
-  //                 url
-  //                 description
-  //                 stargazers {
-  //                   totalCount
-  //                 }
-  //                 forkCount
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // )
+  const {
+    allProjectsJson: { nodes },
+  } = useStaticQuery(
+    graphql`
+      {
+        allProjectsJson {
+          nodes {
+            title
+            tech
+            client
+            links {
+              title
+              url
+            }
+            description
+            id
+            deliverables
+            date {
+              end
+              start
+            }
+          }
+        }
+      }
+    `
+  );
   return (
     <Wrapper as={Container} id="projects">
       <h2>Projects</h2>
       <Grid>
-        {/*{edges.map(({ node }) => (*/}
-        {/*  <Item*/}
-        {/*    key={node.id}*/}
-        {/*    as="a"*/}
-        {/*    href={node.url}*/}
-        {/*    target="_blank"*/}
-        {/*    rel="noopener noreferrer"*/}
-        {/*  >*/}
-        {/*    <Card>*/}
-        {/*      <Content>*/}
-        {/*        <h4>{node.name}</h4>*/}
-        {/*        <p>{node.description}</p>*/}
-        {/*      </Content>*/}
-        {/*      <Stats>*/}
-        {/*        <div>*/}
-        {/*          <img src={starIcon} alt="stars" />*/}
-        {/*          <span>{node.stargazers.totalCount}</span>*/}
-        {/*        </div>*/}
-        {/*        <div>*/}
-        {/*          <img src={forkIcon} alt="forks" />*/}
-        {/*          <span>{node.forkCount}</span>*/}
-        {/*        </div>*/}
-        {/*      </Stats>*/}
-        {/*    </Card>*/}
-        {/*  </Item>*/}
-        {/*))}*/}
+        {nodes.map(node => (
+          <Card {...node}></Card>
+        ))}
       </Grid>
     </Wrapper>
-  )
-}
+  );
+};
