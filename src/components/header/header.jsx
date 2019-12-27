38ed9c1498032639b13ce56a3559b06be54b88b0
Joe Karasek <joseph.karasek@gmail.com>
2019-12-27 10:06:45 -0800

    refactor: header cleanup

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -15,15 +15,36 @@ export const Header = () => (
         <p className="header__item">Tooling</p>
       </div>
       <nav className="header__nav">
-        <a href="#projects" className="header__link">Portfolio</a>
-        <a href="#interests" className="header__link">Interests</a>
-        <a href="#skills" className="header__link">Skills</a>
-        <a href="#talks" className="header__link">Talks</a>
-        <a href="#about" className="header__link">About Me</a>
-        <a href="#contact" className="header__link">Contact</a>
+        <a href="#projects" className="header__link">
+          <img className="header__link-icon" src="/icons/hand-point-down-solid.svg" alt="pointing down" />
+          Portfolio
+        </a>
+        <a href="#interests" className="header__link">
+          <img className="header__link-icon" src="/icons/hand-point-down-solid.svg" alt="pointing down" />
+          Interests
+        </a>
+        <a href="#skills" className="header__link">
+          <img className="header__link-icon" src="/icons/hand-point-down-solid.svg" alt="pointing down" />
+          Skills
+        </a>
+        <a href="#talks" className="header__link">
+          <img className="header__link-icon" src="/icons/hand-point-down-solid.svg" alt="pointing down" />
+          Talks
+        </a>
+        <a href="#about" className="header__link">
+          <img className="header__link-icon" src="/icons/hand-point-down-solid.svg" alt="pointing down" />
+          About Me
+        </a>
+        <a href="#contact" className="header__link">
+          <img className="header__link-icon" src="/icons/hand-point-down-solid.svg" alt="pointing down" />
+          Contact
+        </a>
       </nav>
-      <div className="header__links">
-        Find me on <a href="">Github</a>
+      <div className="header__social">
+        <a href="https://github.com/joekarasek"><img className="header__social-icon" src="/icons/github.svg" alt="Github" /></a>
+        <a href="https://www.linkedin.com/in/joe-karasek/"><img className="header__social-icon" src="/icons/linkedin.svg" alt="LinkedIn" /></a>
+        <a href="https://twitter.com/karasek_joe"><img className="header__social-icon" src="/icons/twitter.svg" alt="Twitter" /></a>
+        <a href="mailto:jozin.joe@gmail.com"><img className="header__social-icon" src="/icons/email.svg" alt="Email" /></a>
       </div>
     </div>
   </header>
