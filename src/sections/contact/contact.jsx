b984d240390024bc78663e849d9d658b910fdbb5
Joe Karasek <joseph.karasek@gmail.com>
2020-03-04 19:18:06 -0500

    feat: contact icons get same treatment as header icons

--- src/sections/contact/contact.jsx
+++ src/sections/contact/contact.jsx
@@ -5,18 +5,18 @@ import './contact.scss';
 
 export const Contact = () => {
   return (
-      <>
-        <span id="contact" />
-        <Headline decoration="left" className="interests__title">Contact Me</Headline>
-        <div className="contact section container">
-          <div className="contact__social">
-            <a href="https://github.com/joekarasek"><img className="contact__social-icon" src="/icons/github.svg" alt="Github" /></a>
-            <a href="https://www.linkedin.com/in/joe-karasek/"><img className="contact__social-icon" src="/icons/linkedin.svg" alt="LinkedIn" /></a>
-            <a href="https://twitter.com/karasek_joe"><img className="contact__social-icon" src="/icons/twitter.svg" alt="Twitter" /></a>
-            <a href="mailto:jozin.joe@gmail.com"><img className="contact__social-icon" src="/icons/email.svg" alt="Email" /></a>
-          </div>
+    <>
+      <span id="contact" />
+      <Headline decoration="left" className="interests__title">Contact Me</Headline>
+      <div className="contact section container">
+        <div className="contact__social">
+          <a className="contact__social-icon" href="https://github.com/joekarasek"><img src="/icons/github.svg" alt="Github" /></a>
+          <a className="contact__social-icon" href="https://www.linkedin.com/in/joe-karasek/"><img src="/icons/linkedin.svg" alt="LinkedIn" /></a>
+          <a className="contact__social-icon" href="https://twitter.com/karasek_joe"><img src="/icons/twitter.svg" alt="Twitter" /></a>
+          <a className="contact__social-icon" href="mailto:jozin.joe@gmail.com"><img src="/icons/email.svg" alt="Email" /></a>
         </div>
-      </>
+      </div>
+    </>
   );
 };
 
