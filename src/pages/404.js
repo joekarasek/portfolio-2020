1de8bf0241ebb49f1ffec2120c42e4b1ff81fa5b
Joe Karasek <joseph.karasek@gmail.com>
2020-03-15 07:08:00 -0700

    feat: custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { SiteMeta } from 'components';
+import { Link } from 'gatsby';
+import { Headline } from '../components';
 
 export default () => (
   <>
     <SiteMeta title="404: Not found" location="/404" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="project-page__home container">
+      <Link to="/"><img src="/icons/home.svg" alt="Homepage" /></Link>
+    </div>
+    <Headline headingType="h1" decoration="left">Dead Link</Headline>
+    <img src="/images/dead-link.png" alt="Dead Link from Zelda game series" style={{ maxWidth: '200px' }} />
   </>
 );
